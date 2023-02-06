50a7ce436a6833e653a3593d065e9cd7f6a2929d
rkritchat <rkritchat@gmail.com>
2023-02-07 01:35:14 +1100

    fix one landmark and add alt for img

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 function App() {
   return (
-    <div className='h-screen w-full bg-[#D6E2EF] flex items-center justify-center font-[Outfit] text-[15px]'>
-      <div className=' bg-[#FFFFFF] rounded-2xl shadow-sm flex flex-col justify-center items-center mx-5'>
+    <main className='h-screen w-full bg-[#D6E2EF] flex items-center justify-center font-[Outfit] text-[15px]'>
+      <section className=' bg-[#FFFFFF] rounded-2xl shadow-sm flex flex-col justify-center items-center mx-5'>
         <div className='rounded-xl overflow-hidden mx-4 my-4'>
-          <img src='/image-qr-code.png' className='w-full h-[18rem]' />
+          <img
+            src='/image-qr-code.png'
+            className='w-full h-[18rem]'
+            alt='img'
+          />
         </div>
         <div className='text-center pb-8 pt-2 w-[20rem] flex flex-col items-center justify-center'>
           <p className='text-xl font-[700] text-[#1F3150] w-[15rem]'>
@@ -14,8 +18,8 @@ function App() {
             skills to the next level
           </p>
         </div>
-      </div>
-    </div>
+      </section>
+    </main>
   )
 }
 
